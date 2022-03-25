31ad923dc4bd7f7da66378bde694ad9788f474f2
Ali Raza Zaidi <alirzaidi02@gmail.com>
2022-03-25 15:07:49 -0400

    DIG-782: Initial Commit; Modifying Quickstart

--- experiments/synthea-breast-cancer/winter2022/Federated/quickstart.sh
+++ experiments/synthea-breast-cancer/winter2022/Federated/quickstart.sh
@@ -10,6 +10,10 @@ PROJECT_NAME="synthea-project"
 DATASET_NAME="synthea-dataset"
 TABLE_NAME="synthea-table"
 
+# Script Kill Constants
+export TOP_PID=$$
+trap "exit 1" TERM
+
 ################################################################################
 # Help                                                                         #
 ################################################################################
@@ -22,15 +26,13 @@ help ()
    echo "Start this program from the root directory of the federated-learning repo"
    echo
    echo "Usage:"
-   echo "   ./quickstart.sh [options] DATA_PATH NUM_SITES BASE_PORT"
-   echo "Arguments:"
-   echo "   DATA_PATH      The absolute path of the Synthea breast cancer dataset on your workstation."
-   echo "   NUM_SITES      The number of federated learning sites/clients to generate"
-   echo "   BASE_PORT      The base port number for the federated-learning docker services"
+   echo "   ./quickstart.sh [options]"
    echo "Options:"
-   echo "   -h      help: Display this help text"
-   echo "   -i      ingest: Ingest Data into Katsu"
-   echo "   -s      same: Keep all of the data in one dataset"
+   echo "   -i <PATH>     Ingest Data present at Path into Katsu"
+   echo "   -p <PORT>     Specify Port Number to expose. Defaults to 5000"
+   echo "   -n <SITES>    Number of Sites to federate. Defaults to 2"
+   echo "   -s            Keep all of the data in one dataset - Useful only if -i specified"
+   echo "   -h            Display this help text"
 }
 
 ################################################################################
@@ -39,26 +41,90 @@ help ()
 error () 
 {
     echo "$1" 1>&2
-    exit 1
+    kill -s TERM $TOP_PID
+}
+
+################################################################################
+# Helper Functions                                                             #
+################################################################################
+check_path()
+{
+    SYNTHEA_PATH=${1}
+    
+    if [[ ${SYNTHEA_PATH} =~ ^-.* ]]; then
+        error "The ingestion PATH '${SYNTHEA_PATH}' cannot be an option"
+    fi
+
+    ls ${SYNTHEA_PATH} 2>/dev/null 1>/dev/null  
+    SYNTHEA_PATH_CODE=$?
+
+    if [[ ${SYNTHEA_PATH_CODE} -ne 0 ]]; then
+        error "There was an error in finding the ingestion PATH '${SYNTHEA_PATH}': Error Code ${SYNTHEA_PATH_CODE}"
+    fi
+
+    if ! [[ ${SYNTHEA_PATH} =~ .*/$ ]]; then
+        SYNTHEA_PATH="${SYNTHEA_PATH}/"
+    fi
+
+    echo ${SYNTHEA_PATH}
+}
+
+check_port()
+{
+    BASE_PORT=${1}
+
+    if ! [[ "${BASE_PORT}" =~ ^[0-9]*$ ]]; then
+        error "The argument PORT must be a positive integer. You entered ${BASE_PORT}"
+    elif ! [[ ${BASE_PORT} -ge 1 ]]; then
+        error "The argument PORT must be greater than 0. You entered ${BASE_PORT}"
+    fi
+
+    echo ${BASE_PORT}
+}
+
+check_sites()
+{
+    NUM_SITES=${1}
+
+    if ! [[ "${NUM_SITES}" =~ ^[0-9]+$ ]]; then
+        error "The option SITES must be a non-negative integer. You entered: ${NUM_SITES}"
+    elif ! [[ ${NUM_SITES} -ge 2 ]]; then
+        error "2 or more clients are required to set up the federated-learning service. You entered: ${NUM_SITES}"
+    fi
+
+    echo ${NUM_SITES}
+}
+
+check_value()
+{
+    value=${1}
+    default=${2}
+
+    if [[ -z ${value} ]]; then
+        value=${default}
+    fi
+
+    echo ${value}
 }
 
 ################################################################################
 # Script                                                                       #
 ################################################################################
 
-# TODO: Change Synthea Path to be an Option Argument (-i SYNTHEA_PATH) instead of Argument
-# TODO: Change Base Port to be an Option (-p) instead of Argument
-# TODO: Create Option (-r) to reset katsu DB before ingestion 
-
 # Read in the script options
-while getopts ":his" opt; do
+while getopts ":i:p:n:sh" opt; do
   case $opt in
+    i)  SYNTHEA_PATH=$(check_path ${OPTARG})
+        to_ingest=1
+        ;;
+    p)  BASE_PORT=$(check_port ${OPTARG})
+        ;;
+    n)  NUM_SITES=$(check_sites ${OPTARG})
+        ;;
+    s)  SAME_DATA=1
+        ;;
     h)  help
         exit 0
-        ;;
-    i)  to_ingest=1
-        ;;
-    s)  same_data=1
         ;;
     \?) echo "Invalid option -$OPTARG" >&2
         exit 0
@@ -67,61 +133,20 @@ while getopts ":his" opt; do
 done
 shift "$((OPTIND - 1))"
 
-if [[ $# -lt 2 ]]; 
-   then 
-   printf "Not enough arguments - entered %d argument(s). Call the script with the -h flag for details.\n" $# 
-   exit 0 
-fi
-
-# Get Arguments & Set Default Values
-SYNTHEA_PATH=$1
-NUM_SITES=$2
-BASE_PORT=$3
-
-if [[ -z to_ingest ]]; then
-    to_ingest=0
-fi
-
-if [[ -z same_data ]]; then
-    same_data=0
-fi
-
-if [[ -z ${BASE_PORT} ]]; then
-    BASE_PORT=5000
-fi
-
-# Validate Arguments
-ls ${SYNTHEA_PATH} 2>/dev/null 1>/dev/null  
-SYNTHEA_PATH_CODE=$?
-
-if [[ ${SYNTHEA_PATH_CODE} -ne 0 ]]; then
-    error "There was an error in finding the SYNTHEA_PATH: Error Code ${SYNTHEA_PATH_CODE}"
-fi
-
-if ! [[ ${SYNTHEA_PATH} =~ .*/$ ]]; then
-    SYNTHEA_PATH="${SYNTHEA_PATH}/"
-fi
-
-if ! [[ "${NUM_SITES}" =~ ^[0-9]+$ ]]; then
-    error "The argument NUM_SITES must be a non-negative integer. You entered: ${NUM_SITES}"
-elif ! [[ ${NUM_SITES} -ge 2 ]]; then
-    error "2 or more clients are required to set up the federated-learning service. You entered: ${NUM_SITES}"
-fi
-
-if ! [[ "${BASE_PORT}" =~ ^[0-9]*$ ]]; then
-    error "The argument BASE_PORT must be a positive integer. You entered ${BASE_PORT}"
-elif ! [[ ${BASE_PORT} -ge 1 ]]; then
-    error "The argument BASE_PORT must be a greater than 0. You entered ${BASE_PORT}"
-fi
+# Get Argument Values & Set Defaults
+SYNTHEA_PATH=$(check_value ${SYNTHEA_PATH} "NOT INGESTING DATA")
+SAME_DATA=$(check_value ${SAME_DATA} 0)
+BASE_PORT=$(check_value ${BASE_PORT} 5000)
+NUM_SITES=$(check_value ${NUM_SITES} 2)
 
 # Display Arguments
 echo "The following arguments have been provided:"
 echo "      SYNTHEA_PATH: ${SYNTHEA_PATH}"
 echo "      NUM_SITES: ${NUM_SITES}"
 echo "      BASE_PORT: ${BASE_PORT}"
-echo
 
 # Create Docker-Compose File
+echo
 $PWD/orchestration-scripts/configure_docker_compose.py ${BASE_PORT} ${NUM_SITES}
 
 # Start Katsu & GraphQL-interface
@@ -143,7 +168,7 @@ if [[ ${to_ingest} -eq 1 ]]; then
     fi
 
     # Ingest Data into one Table
-    if [[ ${same_data} -eq 1 ]]; then
+    if [[ ${SAME_DATA} -eq 1 ]]; then
         echo
         ingestion=$(bash $PWD/ingestion-scripts/init.sh -l -d ${SYNTHEA_PATH} ${PROJECT_NAME} ${DATASET_NAME} ${TABLE_NAME} mcodepacket | tee /dev/tty)
 
